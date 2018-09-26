43debcfaf838335410a0408b43d00df97a70dfcf
Arthur Cabral Fleischman <33187588+TkfleBR@users.noreply.github.com>
2018-09-26 17:24:22 -0300

    #2 sketch made

--- receiver.ino
+++ receiver.ino
@@ -1,10 +1,45 @@
+#include<SPI.h>
 #include<RF24.h>
+#include<Servo.h>
 
-void setup()
+Servo servox;
+Servo servoy;
+
+RF24 radio (7, 8);      
+
+struct package
 {
+  int valbx = 90;             //thats will make the camera allways start in right position 90 degrees 
+  int valby = 90;
+  
+}data;
 
+byte addresses[][6] = {"0"}; 
+
+
+
+void setup() {
+  
+  radio.begin(); 
+  radio.setChannel(115); 
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate( RF24_250KBPS ) ; 
+  radio.readingPipe(1,addresses[0]);
+  radio.startListening();
+  
+  servox.attach(9);
+  servoy.attach(10);
 }
+
 void loop()
 {
-
+  if(radio.available()
+  {
+    while(radio.available())
+    {
+      radio.(&data,sizeof(data));
+      servox.write(data.valbx);
+      servoy.write(data.valby);
+    }
+  }
 }
