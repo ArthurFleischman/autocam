8ad8860cd8abb4f9f5b39da2f261a7776b07e63a
Arthur Cabral Fleischman <33187588+TkfleBR@users.noreply.github.com>
2018-10-02 17:19:07 -0300

    adding comments to emitter.ino

--- emitter.ino
+++ emitter.ino
@@ -2,38 +2,39 @@
 #include<RF24.h>
 
 RF24 radio (7, 8); 
-struct package{
+struct package                                    //struct of data to be sent
+{                                                     
   
   int valbx = 90;
   int valby = 90;
   
 }data;
 
-byte addresses[][6] = {"0"}; 
+byte addresses[][6] = {"0"};                      //addresses
 
-int valx;
+int valx;                                         //initializes values of horizontal and vertical servos
 int valy;
 
 void setup()
 {
-
-  radio.begin(); 
+                                                  //radio setup
+  radio.begin();                                  
   radio.setChannel(115); 
   radio.setPALevel(RF24_PA_MAX);
   radio.setDataRate( RF24_250KBPS ) ; 
   radio.openWritingPipe(addresses[0]);
-  
+                                                  //end of radio setup
 }
 
 void loop()
 {
   
-  valx = analogRead(0);
-  valy = analogRead(1);
+  valx = analogRead(0);                          //potentiometer attached to A0 pin
+  valy = analogRead(1);                          //potentiometer attached to A1 pin
  
-  valx = map(valx,0,1023,0,179);
+  valx = map(valx,0,1023,0,179);                 //mapping to the right values
   valy = map(valy,0,1023,0,179);
 
-    radio.write(&data,sizeof(data));
+  radio.write(&data,sizeof(data));               //sending data
 
 }
